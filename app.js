445800cdb35b1c5173f2a0d158339963d8af0de3
Rodri <rodrigoalvesdemacedo@live.com>
2024-03-29 17:39:55 +0100

    Delete an item and filter task selection

--- app.js
+++ app.js
@@ -40,7 +40,16 @@ function deleteCheck(e){
   // Delete todo
   if(item.classList[0] === 'trash-btn'){
     const todo = item.parentElement;
-    todo.remove();
+    // Animation
+    todo.classList.add('fall');
+    todo.addEventListener('transitionend', function(){
+      todo.remove();
+    })
   }
 
+  // Check mark
+  if(item.classList[0] === 'complete-btn'){
+    const todo = item.parentElement;
+    todo.classList.toggle('completed')
+  }
 }
